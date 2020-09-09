f48cba2aa7102db1d3f3f6ce142c763bbd539cf3
egipe <gipe.elisa@gmail.com>
2020-09-08 22:07:50 -0700

    finished part 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,27 +5,34 @@ var tbody = d3.select("tbody");
 // Select the button
 var button = d3.select("#filter-btn");
 
-// Create event handler
-button.on("click", run_filter)
-
 // Add the table data
 tableData.forEach((user_search)=> {
     var row = tbody.append("tr");
-    Object.defineProperties(user_search).forEach(([key,value]) => {
+    Object.entries(user_search).forEach(([key,value]) => {
         var cell = row.append("td");
         cell.text(value);
     });
 });
 
+// Create event handler
+button.on("click", run_filter);
+
 // Filter Function
 function run_filter() {
     d3.event.preventDefault();
-    var inputElement = d3.select("#datetime")
+    var inputElement = d3.select("#datetime");
     var inputValue = inputElement.property("value");
 
     console.log (inputValue);
-    console.log(tableData);
+
+    tbody.html("")
 
     var filtered_date = tableData.filter(selection => selection.datetime === inputValue);
-    console.log(filtered_date);
-}
+    filtered_date.forEach((user_match) => {
+        var row = tbody.append("tr");
+        Object.entries(user_match).forEach(([key,value])=> {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
